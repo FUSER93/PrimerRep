195fbb10ae0253257ec9a38dac9ac8ad049c849d
Sofía Nuñez <31999837+sofiaanunez@users.noreply.github.com>
2018-06-25 20:22:52 -0300

    Add files via upload

--- faq.php
+++ faq.php
@@ -1,5 +1,5 @@
 <?php
-require_once('Clases/autoload.php');
+require_once('funciones.php');
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
--- registro-ok.php
+++ registro-ok.php
@@ -1,4 +1,4 @@
-<?php require_once('funciones.php'); ?>
+<?php require_once('Clases/autoload.php'); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
